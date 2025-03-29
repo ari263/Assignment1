fa24ac896f144a9bdb1d69753dafb5e9074d2258
ari263 <167206946+ari263@users.noreply.github.com>
2025-03-29 15:18:51 +0530

    Assignment1

---
+++ Task1.py
@@ -0,0 +1,13 @@
+a = input('Enter the first number: ')
+b = input('Enter the second number: ')
+a = int(a)
+b = int(b)
+sum= a + b
+sub= a - b
+mul= a * b
+div= a / b
+
+print('\nAddition:', sum)
+print('Subtraction:', sub)
+print('Multiplication:', mul)
+print('Division:', div)
---
+++ Task2.py
@@ -0,0 +1,5 @@
+a = input('Enter your first name: ')
+b = input('Enter your last name: ')
+
+b += '!'
+print("Hello,", a, b, "Welcome to the Python program.")
